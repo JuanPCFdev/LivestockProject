07b83dfd724774c5554e7d0d065f27f619f1a672
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2023-12-06 10:49:00 -0500

    adding Cattle module

---
+++ app/src/main/java/com/jpdev/livestockproject/ui/Cow/Consult/Adapter/CowAdapter.kt
@@ -0,0 +1,28 @@
+package com.jpdev.livestockproject.ui.Cow.Consult.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jpdev.livestockproject.R
+import com.jpdev.livestockproject.domain.model.Cattle
+
+class CowAdapter(
+    private val cowsList: List<Cattle>,
+    val Keys: List<String>,
+    val userKey: String,
+    val farmKey: String
+) :
+    RecyclerView.Adapter<CowViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CowViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return CowViewHolder(layoutInflater.inflate(R.layout.item_cow, parent, false))
+    }
+
+    override fun getItemCount() = cowsList.size
+
+    override fun onBindViewHolder(holder: CowViewHolder, position: Int) {
+        val item = cowsList[position]
+        val key = Keys[position]
+        holder.bind(item, key, userKey, farmKey)
+    }
+}
---
+++ app/src/main/java/com/jpdev/livestockproject/ui/Cow/Consult/Adapter/CowViewHolder.kt
@@ -0,0 +1,45 @@
+package com.jpdev.livestockproject.ui.Cow.Consult.Adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.jpdev.livestockproject.databinding.ItemCowBinding
+import com.jpdev.livestockproject.domain.model.Cattle
+
+class CowViewHolder(view: View) : ViewHolder(view) {
+
+    private val binding = ItemCowBinding.bind(view)
+    fun bind(cow: Cattle,cowKey:String,user:String,farmKey:String) {
+
+        val context = binding.cvCow.context
+
+        binding.tvMarking.text = cow.marking
+        binding.tvGender.text = cow.gender
+        binding.tvWeight.text = cow.weight.toString()
+
+//        binding.delete.setOnClickListener {
+//            val intent = Intent(context, RegisterCowActivity::class.java)
+//            intent.putExtra("userKey",user)
+//            intent.putExtra("farmKey",farmKey)
+//            intent.putExtra("cowKey",cowKey)
+//            context.startActivity(intent)
+//        }
+//
+//        binding.consult.setOnClickListener {
+//            val intent = Intent(context, RegisterCowActivity::class.java)
+//            intent.putExtra("userKey",user)
+//            intent.putExtra("farmKey",farmKey)
+//            intent.putExtra("cowKey",cowKey)
+//            context.startActivity(intent)
+//        }
+//
+//        binding.edit.setOnClickListener {
+//            val intent = Intent(context, RegisterCowActivity::class.java)
+//            intent.putExtra("userKey",user)
+//            intent.putExtra("farmKey",farmKey)
+//            intent.putExtra("cowKey",cowKey)
+//            context.startActivity(intent)
+//        }
+
+    }
+
+}
