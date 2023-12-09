7802ddcc5f08f3b950a26b9b4c89d4827628aad5
AylinOrjuela <acorjuelal@udistrital.edu.co>
2023-12-09 11:31:08 -0500

    Finance view corrections

--- app/src/main/java/com/jpdev/livestockproject/ui/Finance/BuyActivity.kt
+++ app/src/main/java/com/jpdev/livestockproject/ui/Finance/BuyActivity.kt
@@ -1,10 +1,12 @@
 package com.jpdev.livestockproject.ui.Finance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jpdev.livestockproject.R
 import com.jpdev.livestockproject.data.network.FirebaseInstance
 import com.jpdev.livestockproject.databinding.ActivityBuyBinding
+import com.jpdev.livestockproject.ui.Cow.Lifting.Register.RegisterCowActivity
 
 class BuyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBuyBinding
@@ -19,6 +21,22 @@ class BuyActivity : AppCompatActivity() {
     }
 
     private fun initComponents(key: String?, farmKey: String?) {
+        binding.btnReturnHome.setOnClickListener {
+            val intent = Intent(this, FinanceActivity::class.java)
+            intent.putExtra("userKey",key)
+            intent.putExtra("farmKey",farmKey)
+            startActivity(intent)
+            finish()
+        }
+        binding.btnBuyCow.setOnClickListener {
+            val intent = Intent(this, RegisterCowActivity::class.java)
+            intent.putExtra("userKey",key)
+            intent.putExtra("farmKey",farmKey)
+            startActivity(intent)
+            finish()
+        }
+        binding.btnSaveBuy.setOnClickListener {
 
+        }
     }
 }
