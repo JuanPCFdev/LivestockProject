a61dfd2ce016588c0e3726cf4fcfd95ee5be804c
AylinOrjuela <acorjuelal@udistrital.edu.co>
2024-01-11 12:18:35 -0500

    Changes in interface and calculation of profits and losses

--- app/src/main/java/com/jpdev/livestockproject/ui/Finance/EarningLostReceiptActivity.kt
+++ app/src/main/java/com/jpdev/livestockproject/ui/Finance/EarningLostReceiptActivity.kt
@@ -3,6 +3,7 @@ package com.jpdev.livestockproject.ui.Finance
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.jpdev.livestockproject.R
 import com.jpdev.livestockproject.data.network.FirebaseInstance
 import com.jpdev.livestockproject.databinding.ActivityBuyBinding
@@ -13,6 +14,7 @@ class EarningLostReceiptActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEarningLostReceiptBinding
     private lateinit var firebaseInstance: FirebaseInstance
+    private var cost: Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEarningLostReceiptBinding.inflate(layoutInflater)
@@ -32,7 +34,36 @@ class EarningLostReceiptActivity : AppCompatActivity() {
             intent.putExtra("userKey",key)
             intent.putExtra("farmKey",farmKey)
             startActivity(intent)
+
             finish()
         }
+
+        calculateFinances(key, farmKey)
+
+    }
+    private fun calculateFinances(key: String?, farmKey: String?){
+        firebaseInstance.getTotalVaccineCostForAllCows(key,farmKey){totalVaccineCost ->
+            binding.etVaccine.text = totalVaccineCost.toString()
+            firebaseInstance.getSumOfAmountPaidByReceiptType(key,farmKey){ totalAmountPaidCompra, totalAmountPaidVenta ->
+                Log.d("TotalAmountPaidCompra", "Total amount paid for compra: $totalAmountPaidCompra")
+                Log.d("TotalAmountPaidVenta", "Total amount paid for venta: $totalAmountPaidVenta")
+                binding.etWon.text = totalAmountPaidVenta.toString()
+                binding.etinvestment.text = totalAmountPaidCompra.toString()
+
+                cost = totalAmountPaidVenta - (totalAmountPaidCompra + totalVaccineCost)
+
+                if (cost == 0.0 ){
+                    binding.etAbstract.text = getString(R.string.AbstractNothing)
+                }else if (cost > 0.0){
+                    binding.etAbstract.text = getString(R.string.AbstractWon)+" $cost"
+                }else if (cost< 0.0){
+                    binding.etAbstract.text = getString(R.string.AbstractLost)+" $cost"
+                }
+
+            }
+        }
+
     }
 }
+
+
