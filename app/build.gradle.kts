d135b0e4ef102ddd45cab350e491a3058586bc8a
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2024-02-06 22:32:59 -0500

    graphic library and first implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database-ktx")
+    //Graphplot
+    implementation("com.androidplot:androidplot-core:1.5.10")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
